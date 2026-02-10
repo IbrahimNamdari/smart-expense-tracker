2dc4ede1abb1d2f6a400338158459f9359e6b090
IbrahimNamdari <Ibrahimnamdari1999@yahoo.com>
2026-02-11 02:54:29 +0330

    feat: implement CRUD endpoints for expenses and pydantic schemas

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,34 @@
-from fastapi import FastAPI
-import models
-from database import engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+import models, schemas, database
 
 app = FastAPI()
 
-# این خط جادویی است: تمام مدل‌هایی که در models.py ساختی را در دیتابیس آنلاین می‌سازد
-models.Base.metadata.create_all(bind=engine)
+# ساخت جدول‌ها (اگر قبلا ساخته نشده باشند)
+models.Base.metadata.create_all(bind=database.engine)
+
+# تابع کمکی برای وصل شدن به دیتابیس
+def get_db():
+    db = database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @app.get("/")
 def home():
-    return {"message": "Welcome to AI Expense Tracker API!"}
+    return {"message": "API is running!"}
+
+# ۱. ذخیره هزینه جدید
+@app.post("/expenses/", response_model=schemas.ExpenseResponse)
+def create_expense(expense: schemas.ExpenseCreate, db: Session = Depends(get_db)):
+    db_expense = models.Expense(**expense.dict())
+    db.add(db_expense)
+    db.commit()
+    db.refresh(db_expense)
+    return db_expense
+
+# ۲. دریافت لیست تمام هزینه‌ها
+@app.get("/expenses/")
+def read_expenses(db: Session = Depends(get_db)):
+    return db.query(models.Expense).all()
